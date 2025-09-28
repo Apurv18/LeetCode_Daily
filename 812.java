b75aed9b0dc3df703f8e64687f91cd74a0a412f8
ApurvWadnere <wadnereapurv@gmail.com>
2025-09-28 22:20:31 +0530

    4 commits

---
+++ 812.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double largestTriangleArea(int[][] points) {
+        double ans  = Integer.MIN_VALUE;
+        int n = points.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                for(int k = 0;k<n;k++){
+                    int x1 = points[i][0];
+                    int y1 = points[i][1];
+                    int x2 = points[j][0];
+                    int y2 = points[j][1];
+                    int x3 = points[k][0];
+                    int y3 = points[k][1];
+
+                    double max  = 0.5 * Math.abs(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
+                    ans = Math.max(max,ans);
+
+                }
+            }
+        }
+        return ans;
+    }
+}
